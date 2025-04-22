99c89540e150fe4edd6e411b2e00abfd0cafe662
unknown <pnkjdaiict@gmail.com>
2025-04-22 15:45:58 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -6,74 +6,33 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 const nextConfig: NextConfig = {
   /* config options here */
-  
-  experimental : {
-    "webpackMemoryOptimizations" : true
-  } ,
+
+  experimental: {
+    webpackMemoryOptimizations: true
+  },
   images: {
-    
-    domains: [         
-      "127.0.0.1"  ,
-      "scontent.cdninstagram.com",   
+    domains: [
+      "127.0.0.1",
+      "scontent.cdninstagram.com",
       "65.2.63.7",
-      "3.110.189.42", 
+      "3.110.189.42",
       "13.203.78.165",
-      "rdabucket.s3.amazonaws.com" ,
+      "rdabucket.s3.amazonaws.com",
       "43.204.144.192"
     ],
-    // : true,
- 
-  
-  },  
+  },
   typescript: {
-    ignoreBuildErrors: true, 
-  }, 
-  async rewrites() {
+    ignoreBuildErrors: true,
+  },
+  async redirects() {
     return [
-      // Rule 1: Map `/course/:slug` to `/slug`
-      // {
-      //   source: "https://royaldefenceacademy.com/:path*",
-      //   destination: "https://www.royalfenceacademy.com/:path*",
-      //   permanent: true, // 301 redirect
-      // },
       {
-        source: '/blogs/:slug',
-        destination: '/blogs/:slug',
-      },
-      {
-        source: '/news/:slug',
-        destination: '/news/:slug',
-      },
-      {
-        source: '/indexingText/:slug',
-        destination: '/indexingText/:slug',
-      },
-      {
-        source: '/:slug',
-        
-        destination: '/course/:slug',
-      },
-
-      // Nested paths under `/course/:slug/:path*`
-      {
-        source: '/:slug/:path*',
-        destination: '/course/:slug/:path*',
-      },
-      // {
-      //   source: '/:slug',
-      //   destination: '/course/:slug', // `/slug` dynamic page
-      // },
-      // // Rule 2: Map `/course/:slug/:path*` to `/slug/:path*`
-      // {
-      //   source: '/:slug/:path*',
-      //   destination: '/course/:slug/:path*', // `/slug` dynamic page
-      // },
- 
+        source: "/(.*)",
+        destination: "https://militaryschoolscoaching.com/:path*",
+        permanent: true,
+      }
     ];
   }
-  
-  
-  
 };
 
 export default nextConfig;
